66db5d520f12d14de42152df69dfde568bfcdcfb
Ritesh Takawale <163227165+Ritesh-Takawale@users.noreply.github.com>
2026-04-02 00:15:58 +0530

    Update Vite config for server and preview settings

--- portfolio_builder/vite.config.js
+++ portfolio_builder/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    host: true
+  },
   preview: {
     host: true,
-    port: 10000,
-    allowedHosts: 'all'
+    port: process.env.PORT || 10000,
+    allowedHosts: true
   }
 })
